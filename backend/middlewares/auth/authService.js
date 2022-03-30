d4ad83ec9ec077d66fcf15cff156d55c15de1ec2
rachaen <rachaenlee@gmail.com>
2022-03-30 16:26:53 +0900

    유효성 검사 수정 + 중복체크 axios 추가

--- backend/middlewares/auth/authService.js
+++ backend/middlewares/auth/authService.js
@@ -1,8 +1,8 @@
-const { v4 } = require("uuid");
-const bcrypt = require("bcrypt");
+const { v4 } = require('uuid');
+const bcrypt = require('bcrypt');
 
-const authRepository = require("./authRepository");
-const config = require("../../config/config.js");
+const authRepository = require('./authRepository');
+const config = require('../../config/config.js');
 
 const authService = {
   /**
@@ -16,7 +16,7 @@ const authService = {
     const findByEmail = await authRepository.findByEmail(email);
     const findPhoneNumber = await authRepository.findPhoneNumber(phoneNumber);
     if (findByEmail && findNickName && findPhoneNumber) {
-      res.status(409).json({ message: "회원 가입에 실패했습니다. 중복체크를 다시 확인해주세요" });
+      res.status(409).json({ message: '회원 가입에 실패했습니다. 중복체크를 다시 확인해주세요' });
     }
     const hashed = await bcrypt.hash(password, config.bcrypt.saltRounds);
     const signupResult = await authRepository.signup({
@@ -32,9 +32,9 @@ const authService = {
       longitude,
     });
     if (!signupResult) {
-      res.status(401).json({ message: "회원 가입에 실패했습니다" });
+      res.status(401).json({ message: '회원 가입에 실패했습니다' });
     }
-    res.status(200).json({ message: "회원 가입에 성공했습니다" });
+    res.status(200).json({ message: '회원 가입에 성공했습니다' });
   },
 
   /**
@@ -43,9 +43,9 @@ const authService = {
   nickNameDuplicateCheck: async (req, res) => {
     const findByNickName = await authRepository.findByNickName(req.query.nickName);
     if (findByNickName) {
-      res.status(200).json({ message: "중복된 닉네임 입니다" });
+      res.status(200).json({ message: '중복된 닉네임 입니다' });
     }
-    res.status(200).json({ message: "사용 가능한 닉네임 입니다" });
+    res.status(200).json({ message: '사용 가능한 닉네임 입니다' });
   },
 
   /**
@@ -54,9 +54,9 @@ const authService = {
   emailDuplicateCheck: async (req, res) => {
     const findByEmail = await authRepository.findByEmail(req.query.email);
     if (findByEmail) {
-      res.status(200).json({ message: "중복된 이메일 입니다" });
+      res.status(200).json({ message: '중복된 이메일 입니다' });
     }
-    res.status(200).json({ message: "사용 가능한 이메일 입니다" });
+    res.status(200).json({ message: '사용 가능한 이메일 입니다' });
   },
 
   /**
@@ -65,9 +65,9 @@ const authService = {
   phoneNumberDuplicateCheck: async (req, res) => {
     const findByPhoneNumber = await authRepository.findPhoneNumber(req.query.phoneNumber);
     if (findByPhoneNumber) {
-      res.status(200).json({ message: "중복된 휴대폰번호 입니다" });
+      res.status(200).json({ message: '중복된 휴대폰번호 입니다' });
     }
-    res.status(200).json({ message: "사용 가능한 휴대폰번호 입니다" });
+    res.status(200).json({ message: '사용 가능한 휴대폰번호 입니다' });
   },
 };
 
--- frontend/src/components/signup/signup.jsx
+++ frontend/src/components/signup/signup.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Signup = (props) => {
   const [userRegistration, setUserRegistration] = useState({
@@ -13,13 +14,17 @@ const Signup = (props) => {
     longitude: '',
   });
 
+  // true면 통과
   const [error, setError] = useState({
     email: false, // 이메일 유효성
     emailDuplicate: false, // 이메일 중복
+    nickName: false, // 닉네임 유효성
     nickNameDuplicate: false, // 닉네임 중복
     password: false, // 비밀번호 유효성
     password2: false, // 비밀번호 확인
-    phoneVerification: false, // 휴대폰 인증
+    phoneNumber: false,
+    phoneNumberDuplicate: false, // 휴대폰 중복
+    phoneNumberVerification: false, // 휴대폰 인증
   });
 
   const [password2, setPassword2] = useState('');
@@ -28,7 +33,10 @@ const Signup = (props) => {
     const {
       target: { name, value },
     } = event;
-    setUserRegistration((userRegistration) => ({ ...userRegistration, [name]: value }));
+    setUserRegistration((userRegistration) => ({
+      ...userRegistration,
+      [name]: value,
+    }));
   };
 
   const handlePassword2 = (event) => {
@@ -41,36 +49,158 @@ const Signup = (props) => {
   useEffect(() => {
     let emailExp = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
     if (userRegistration.email && emailExp.test(userRegistration.email) === false) {
-      setError((error) => ({ ...error, email: true }));
+      setError((error) => ({
+        ...error,
+        email: false,
+      }));
     } else {
-      setError((error) => ({ ...error, email: false }));
+      setError((error) => ({
+        ...error,
+        email: true,
+      }));
     }
   }, [userRegistration.email]);
 
   useEffect(() => {
-    let passwordExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,12}$/;
-    if (userRegistration.password && passwordExp.test(userRegistration.password) === false) {
-      setError((error) => ({ ...error, password: true }));
+    let nickNameExp = /^[가-힣A-Za-z0-9_]{2,7}$/;
+    if (userRegistration.nickName && nickNameExp.test(userRegistration.nickName) === false) {
+      setError((error) => ({
+        ...error,
+        nickName: false,
+      }));
     } else {
-      setError((error) => ({ ...error, password: false }));
+      setError((error) => ({
+        ...error,
+        nickName: true,
+      }));
+    }
+  }, [userRegistration.nickName]);
+
+  useEffect(() => {
+    let passwordExp = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
+    if (userRegistration.password && passwordExp.test(userRegistration.password) === false) {
+      setError((error) => ({
+        ...error,
+        password: false,
+      }));
+    } else {
+      setError((error) => ({
+        ...error,
+        password: true,
+      }));
+    }
+  }, [userRegistration.password]);
+
+  useEffect(() => {
+    let phoneNumberExp = /^\d{10,11}$/;
+    if (userRegistration.password && phoneNumberExp.test(userRegistration.phoneNumber) === false) {
+      setError((error) => ({
+        ...error,
+        phoneNumber: false,
+      }));
+    } else {
+      setError((error) => ({
+        ...error,
+        phoneNumber: true,
+      }));
     }
   }, [userRegistration.password]);
 
   // 비밀번호 일치 검사
   useEffect(() => {
     if (userRegistration.password !== password2) {
-      setError((error) => ({ ...error, password2: true }));
+      setError((error) => ({
+        ...error,
+        password2: false,
+      }));
     } else {
-      setError((error) => ({ ...error, password2: false }));
+      setError((error) => ({
+        ...error,
+        password2: true,
+      }));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [password2]);
 
-  const nickNameCheck = (event) => {};
+  // 중복검사
+  const emailCheck = (event) => {
+    event.preventDefault();
+    if (!error.email || userRegistration.email === '') {
+      alert('이메일 형식을 맞춰주세요');
+      return;
+    }
+    axios.get(`http://127.0.0.1:8080/auth/emailDuplicateCheck?email=${userRegistration.email}`).then((result) => {
+      if (result.status === 200) {
+        if (result.data.result) {
+          setError({
+            ...error,
+            emailDuplicate: true,
+          });
+        } else {
+          setError({
+            ...error,
+            emailDuplicate: false,
+          });
+        }
+      } else {
+        console.log('뭔가 잘못되었당');
+      }
+    });
+  };
 
-  const emailCheck = (event) => {};
+  const nickNameCheck = (event) => {
+    event.preventDefault();
+    if (!error.nickName || userRegistration.nickName === '') {
+      alert('닉네임 형식을 맞춰주세요');
+      return;
+    }
+    axios
+      .get(`http://127.0.0.1:8080/auth/phoneNumberDuplicateCheck?nickName=${userRegistration.nickName}`)
+      .then((result) => {
+        if (result.status === 200) {
+          if (result.data.result) {
+            setError({
+              ...error,
+              nickNameDuplicate: true,
+            });
+          } else {
+            setError({
+              ...error,
+              nickNameDuplicate: false,
+            });
+          }
+        } else {
+          console.log('뭔가 잘못되었당');
+        }
+      });
+  };
 
-  const phoneNumberCheck = (event) => {};
+  const phoneNumberCheck = (event) => {
+    event.preventDefault();
+    if (!error.phoneNumber || userRegistration.phoneNumber === '') {
+      alert('휴대폰 형식을 맞춰주세요');
+      return;
+    }
+    axios
+      .get(`http://127.0.0.1:8080/auth/nickNameDuplicateCheck?nickName=${userRegistration.nickName}`)
+      .then((result) => {
+        if (result.status === 200) {
+          if (result.data.result) {
+            setError({
+              ...error,
+              phoneNumberDuplicate: true,
+            });
+          } else {
+            setError({
+              ...error,
+              phoneNumberDuplicate: false,
+            });
+          }
+        } else {
+          console.log('뭔가 잘못되었당');
+        }
+      });
+  };
 
   // 회원가입
   const handleSubmit = (event) => {
@@ -81,31 +211,69 @@ const Signup = (props) => {
     <form>
       <div>
         <label>이메일</label>
-        <input name="email" type="text" placeholder="이메일" required value={userRegistration.email} onChange={handleInput} />
+        <input name="email" type="text" placeholder="이메일" value={userRegistration.email} onChange={handleInput} />
         <button onClick={emailCheck}>중복확인</button>
-        <span>{userRegistration.email && (error.email ? '유효성 불통' : '유효성 통')}</span>
+        {/* <span>{userRegistration.email && (error.email ? '' : '이메일 형식을 지켜주세요')}</span> */}
+        <span>
+          {userRegistration.email &&
+            (error.email ? (error.emailDuplicate ? '사용 가능한 닉네임입니다.' : '') : '이메일 형식을 지켜주세요')}
+        </span>
       </div>
       <div>
         <label>비밀번호</label>
-        <input name="password" type="password" placeholder="비밀번호" required value={userRegistration.password} onChange={handleInput} />
-        <span>{userRegistration.password && (error.password ? '유효성 불통' : '유효성 통')}</span>
+        <input
+          name="password"
+          type="password"
+          placeholder="비밀번호"
+          maxLength="20"
+          value={userRegistration.password}
+          onChange={handleInput}
+        />
+        <span>
+          {userRegistration.password &&
+            (error.password ? '안전한 비밀번호 입니다.' : '영문, 숫자, 특수기호 조합으로 8-20자리 이상 입력해주세요')}
+        </span>
       </div>
       <div>
         <label>비밀번호 확인</label>
-        <input name="password2" type="password" placeholder="비밀번호" required value={password2} onChange={handlePassword2} />
-        {/* <span>{passwordMessage()}</span> */}
-        {/* {message.password2 && <span>{message.password2}</span>} */}
-        <span>{password2 && (error.password2 ? '안돼' : '됨')}</span>
+        <input
+          name="password2"
+          type="password"
+          placeholder="비밀번호"
+          maxLength="20"
+          value={password2}
+          onChange={handlePassword2}
+        />
+        <span>{password2 && (error.password2 ? '비밀번호를 다시 확인해주세요' : 'ok')}</span>
       </div>
       <div>
         <label>휴대폰 번호</label>
-        <input name="phoneNumber" type="text" placeholder="휴대폰 번호" required value={userRegistration.name} onChange={handleInput} />
+        <input
+          name="phoneNumber"
+          type="text"
+          placeholder="휴대폰 번호"
+          maxLength="11"
+          value={userRegistration.phoneNumber}
+          onChange={handleInput}
+        />
         <button onClick={phoneNumberCheck}>인증하기</button>
+        <span>{userRegistration.phoneNumber && (error.phoneNumberVerification ? '인증 완료' : '')}</span>
       </div>
       <div>
         <label>닉네임</label>
-        <input name="nickName" type="text" placeholder="닉네임" value={userRegistration.nickName} onChange={handleInput} />
+        <input
+          name="nickName"
+          type="text"
+          placeholder="닉네임"
+          maxlength="5"
+          value={userRegistration.nickName}
+          onChange={handleInput}
+        />
         <button onClick={nickNameCheck}>check</button>
+        <span>
+          {userRegistration.nickName &&
+            (error.nickName ? (error.nickNameDuplicate ? '사용 가능한 닉네임입니다.' : '') : '2자리 이상')}
+        </span>
       </div>
       <div>
         <label>주소</label>
